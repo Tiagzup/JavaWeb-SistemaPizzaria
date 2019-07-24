7aa574aba353af57722af7226ed2004cfb60ec8a
Tiago Souza <tiago.souza@zup.com.br>
2019-07-24 18:44:59 -0300

    Adicionar atributos pizza

--- com.br.zup.pizza/src/main/java/com/br/zup/model/PizzaModel.java
+++ com.br.zup.pizza/src/main/java/com/br/zup/model/PizzaModel.java
@@ -1,5 +1,23 @@
 package com.br.zup.model;
 
-public class PizzaModel {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class PizzaModel implements Serializable {
+	public static final long serialVersionUID = 1l;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String sabor;
+	private String massa;
+	private String tipoDeBorda;
+	private int valor;
 }
